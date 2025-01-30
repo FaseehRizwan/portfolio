65052eb003874528d4eb3658f13c693a4d8d24ba
Muhammad Faseeh Rizwan <77075444+FaseehRizwan@users.noreply.github.com>
2025-01-31 03:49:27 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,5 +99,8 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-
+// Mobile Menu Toggle
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('active');
+});
 
